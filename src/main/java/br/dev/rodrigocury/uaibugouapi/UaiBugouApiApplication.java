cc74a3d6dec1b4c8aefeaa395b449b792f2303eb
= <=>
2021-10-16 22:38:47 -0300

    Projeto base

---
+++ src/main/java/br/dev/rodrigocury/uaibugouapi/UaiBugouApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigocury.uaibugouapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaiBugouApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaiBugouApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/rodrigocury/uaibugouapi/UaiBugouApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigocury.uaibugouapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaiBugouApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
