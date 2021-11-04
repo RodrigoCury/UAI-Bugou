e2d46fea228560a1771c5eec203333aa520c6b19
= <rodrigocury@ufu.br>
2021-11-04 12:50:10 -0300

    Added Find by Email

--- src/main/java/br/dev/rodrigocury/uaibugouapi/repositories/FuncionarioRepository.java
+++ src/main/java/br/dev/rodrigocury/uaibugouapi/repositories/FuncionarioRepository.java
@@ -3,5 +3,9 @@ package br.dev.rodrigocury.uaibugouapi.repositories;
 import br.dev.rodrigocury.uaibugouapi.models.funcionario.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+  Optional<Funcionario> findByFuncionarioEmail(String email);
 }
