1cf3b54fe5b04c45b4dba30285abc04efab21b63
= <rodrigocury@ufu.br>
2021-11-04 12:07:02 -0300

    Controller para rotas relacionadas a empresa

---
+++ src/main/java/br/dev/rodrigocury/uaibugouapi/controllers/EmpresaController.java
@@ -0,0 +1,32 @@
+package br.dev.rodrigocury.uaibugouapi.controllers;
+
+import br.dev.rodrigocury.uaibugouapi.dto.NovaEmpresaDto;
+import br.dev.rodrigocury.uaibugouapi.forms.NovaEmpresaForm;
+import br.dev.rodrigocury.uaibugouapi.services.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/empresa")
+public class EmpresaController {
+
+  private final EmpresaService empresaService;
+
+  @Autowired
+  public EmpresaController(EmpresaService empresaService){
+    this.empresaService = empresaService;
+  }
+
+  @PostMapping
+  public ResponseEntity<NovaEmpresaDto> criaEmpresaEAdm(@Valid @RequestBody NovaEmpresaForm form){
+    NovaEmpresaDto novaEmpresaDto = empresaService.criaEmpresa(form);
+    return ResponseEntity.ok(novaEmpresaDto);
+  }
+
+}
