af2168ce3c355c1ef113d77547123e6644c4e6b9
= <rodrigocury@ufu.br>
2021-11-05 12:23:37 -0300

    Update AuthenticationHelper.java

--- src/main/java/br/dev/rodrigocury/uaibugouapi/utils/AuthenticationHelper.java
+++ src/main/java/br/dev/rodrigocury/uaibugouapi/utils/AuthenticationHelper.java
@@ -1,5 +1,6 @@
 package br.dev.rodrigocury.uaibugouapi.utils;
 
+import br.dev.rodrigocury.uaibugouapi.models.entidadesdaempresa.Empresa;
 import br.dev.rodrigocury.uaibugouapi.models.funcionario.Funcionario;
 import org.springframework.security.core.Authentication;
 
@@ -7,5 +8,9 @@ public class AuthenticationHelper {
 
   public static Funcionario getAutenticado(Authentication auth) {
     return (Funcionario) auth.getPrincipal();
+  }
+
+  public static Empresa getEmpresaDoAutenticado(Authentication auth) {
+    return AuthenticationHelper.getAutenticado(auth).getEmpresa();
   }
 }
