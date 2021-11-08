7864ba4bcc0b7e2b1aac88ec43d339e7b15040f9
= <rodrigocury@ufu.br>
2021-11-08 12:53:20 -0300

    Enabled Caching

--- src/main/java/br/dev/rodrigocury/uaibugouapi/UaiBugouApiApplication.java
+++ src/main/java/br/dev/rodrigocury/uaibugouapi/UaiBugouApiApplication.java
@@ -2,8 +2,10 @@ package br.dev.rodrigocury.uaibugouapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class UaiBugouApiApplication {
 
 	public static void main(String[] args) {
